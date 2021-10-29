4d9dbe28b62b405840a04aa3123e5a2c83e87322
Henrique Dezani <dezani@gmail.com>
2021-10-29 12:14:06 +0000

    Finaliza projeto com página web

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+import pickle 
+
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+with open('vetorizador.pkl', 'rb') as file_vectorizer:
+    vectorizer = pickle.load(file_vectorizer)
+
+with open('classificador.pkl', 'rb') as file_classificador:
+    classificador = pickle.load(file_classificador)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/classificador', methods=['POST'])
+def cassificador():
+    frase = request.form['texto']
+    vetor = vectorizer.transform([frase])
+    classificacao = 'Positivo' if classificador.predict(vetor)[0] == 1 else 'Negativo'
+    return render_template('resultado.html', frase=frase, classificacao=classificacao)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
